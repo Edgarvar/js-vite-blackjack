2d7d30ac36916c33e2e07f6af66317ef74274ed7
Edgarvar <edgardo_vargas@outlook.com.ar>
2023-05-23 21:17:07 -0300

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearCartaHtml } from './crearCartaHtml';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './perdirCarta';
+export { turnoComputadora } from './turnoComputadora'
+export { valorCarta } from './valorCarta';
